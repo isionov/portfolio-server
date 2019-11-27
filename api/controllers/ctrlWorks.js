a6be5324e690d869fd0a7327b7bb5bd880e6224b
Илья Ионов <ilaionov@MacBook-Pro-Ila.local>
2019-11-28 00:38:21 +0300

    Fix error in admin

--- api/controllers/ctrlWorks.js
+++ api/controllers/ctrlWorks.js
@@ -54,29 +54,15 @@ module.exports.createWork = (req, res) => {
 };
 
 module.exports.changeWork = (req, res) => {
-  console.dir("Hello word");
-  console.dir(path.join(process.cwd(), upload));
-
   const Works = mongoose.model("Works");
   const form = new formidable.IncomingForm();
   const id = req.params.id;
-  console.dir("Hello word 2");
 
   form.uploadDir = path.join(process.cwd(), upload);
-  console.dir("Hello word 3");
-
   form.hash = "md5";
-  console.dir("Hello word 4");
 
   form.parse(req, function(err, fields, file) {
-    console.dir("Hello word 5");
-
     const { title, techs, link, description, photo } = fields;
-    console.dir("Hello word 5");
-
-    console.dir(file);
-    console.dir(file.photo);
-    console.dir(file.photo.path);
 
     if (err) {
       return res
@@ -97,12 +83,9 @@ module.exports.changeWork = (req, res) => {
           let publicPathPhoto;
 
           if (file.photo) {
-            console.dir(file);
             let imageFile = fs.readFileSync(file.photo.path);
-            console.dir("imageFile");
 
             publicPathPhoto = Buffer.from(imageFile).toString("base64");
-            console.dir("publicPathPhoto", publicPathPhoto);
           }
 
           item.title = title;
